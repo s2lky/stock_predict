3a9f49db5f4fbad8c7c77ea48058ef29bd573265
ddangeun123 <133068471+ddangeun123@users.noreply.github.com>
2023-09-26 13:48:10 +0900

    add Comments

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ from flask import Flask, render_template, request, redirect, url_for, session
 from Routes import auth, chat, dashboard
 
 app = Flask(__name__)
-app.register_blueprint(auth.app)
-app.register_blueprint(chat.app)
-app.register_blueprint(dashboard.app)
+app.register_blueprint(auth.app) # auth.py 라우터 가져오기
+app.register_blueprint(chat.app) # chat.py 라우터 가져오기
+app.register_blueprint(dashboard.app) # dashboard.py 라우터 가져오기
 
 if __name__ == "__main__":
     app.run(debug=True, host='localhost', port=8000)
