62e673e37d31d40c938b0a75cfb8d2f3f8c9f617
s2lky <132236456+s2lky@users.noreply.github.com>
2024-02-12 13:41:18 +0900

    Delete Routes directory

--- Routes/chat.py
+++
@@ -1,8 +0,0 @@
-from flask import render_template, session, redirect, url_for, Blueprint
-
-app = Blueprint("chat", __name__)
-
-@app.route('/chat')
-def chat():
-    print("chat")
-    return render_template('chat.html')
--- Routes/dashboard.py
+++
@@ -1,8 +0,0 @@
-from flask import render_template, session, redirect, url_for, Blueprint
-
-app = Blueprint("dashboard", __name__)
-
-@app.route('/dashboard')
-def dashboard():
-    print("dashboard")
-    return render_template('dashboard.html')
